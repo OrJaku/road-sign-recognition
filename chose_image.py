5382c87f4f2ee40cdc8ae565c3ee8cc56f2d96b0
OrJaku <kuba.orzel@hotmail.com>
2020-04-29 23:56:11 +0200

    Few changes in files"

--- chose_image.py
+++ chose_image.py
@@ -2,9 +2,21 @@ import os
 import csv
 import shutil
 
-# label_of_sign = "information--pedestrians-crossing--g1" #znak przejścia dla pieszych
-label_of_sign = "regulatory--stop--g1" #znak stop
-# label_of_sign = "regulatory--stop--g10" #znak stop2
+# label_of_sign_1 = "information--pedestrians-crossing--g1" # znak przejścia dla pieszych
+# label_of_sign_2 = "regulatory--stop--g1" # znak stop
+# label_of_sign_ = "regulatory--stop--g10" # znak stop2
+# label_of_sign = "regulatory--maximum-speed-limit-50--g1"  #znak ograniczenia predkosco do 50km/h
+# label_of_sign = "warning--pedestrians-crossing--g5" # znak przejscia dla pieszych ostrzeg. 
+# label_of_sign = "warning--railroad-crossing--g3" # przejazd kolejowy
+# label_of_sign = "warning--railroad-crossing-with-barriers--g1" # przejazd kolejowy z barierami  
+
+labels_list = [
+                "regulatory--stop--g1",
+                "regulatory--maximum-speed-limit-50--g1",
+                "warning--pedestrians-crossing--g5",
+                "warning--railroad-crossing--g3",
+                "warning--railroad-crossing-with-barriers--g1",
+]
 
 
 local_path = os.path.abspath(os.path.dirname(__file__))
@@ -12,52 +24,53 @@ external_path = "\\\\Dell-komputer\img_mgr\mtsd_fully_annotated"
 path_to_images = os.path.join(external_path, 'images')
 path_to_labels = os.path.join(external_path, 'annotations')
 
-output_files = os.path.join(local_path, 'output')
-print("Output path", output_files)
-folder_with_filtered_images = os.path.join(external_path, f'my_image/{label_of_sign}')
-files_lable_list = os.listdir(path_to_labels)
-files_list_with_csv = os.listdir(output_files)
-print(files_list_with_csv)
-signs_csv_img_list = []
-i = 1
-j = 1
-k = 1
+for label_of_sign in labels_list:
+    output_files = os.path.join(local_path, 'output')
+    print("Output path", output_files)
+    folder_with_filtered_images = os.path.join(external_path, f'my_image/{label_of_sign}')
+    try:
+        os.mkdir(folder_with_filtered_images)
+    except FileExistsError:
+        print(f"DIr {label_of_sign} exist")
+        continue
+    files_lable_list = os.listdir(path_to_labels)
+    files_list_with_csv = os.listdir(output_files)
+    print(files_list_with_csv)
+    signs_csv_img_list = []
+    i = 1
+    j = 1
+    k = 1
 
-# for i in 
-# respond = input("Chose one: ")
+    with open(f'output/imagelist_label_{label_of_sign}.csv', mode="r") as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        line_count = 0
+        for name in csv_file:
+            k += 1
+            name_img = name[:-1]
+            signs_csv_img_list.append(name_img)
+            print(name_img)
+            print(f"Converted: {k} files")
+        print(signs_csv_img_list)
 
-# while respond != "0":
-#     if respond == i
-with open(f'output/imagelist_label_{label_of_sign}.csv', mode="r") as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=',')
-    line_count = 0
-    for name in csv_file:
-        k += 1
-        name_img = name[:-1]
-        signs_csv_img_list.append(name_img)
-        print(name_img)
-        print(f"Converted: {k} files")
-    print(signs_csv_img_list)
-
-for img_name in files_lable_list:
-    j += 1
-    print(f"Checked: {j} files")
-    
-    img_name = img_name[:-5]
-    img_json_name = img_name + ".json"
-    img_jpg_name = img_name + ".jpg"  
-    if img_name in signs_csv_img_list:
-        i += 1
-        shutil.copy(
-                os.path.join(
-                            path_to_images, img_jpg_name), 
-                            folder_with_filtered_images
-                            )
-        shutil.copy(
-                os.path.join(
-                            path_to_labels, img_json_name), 
-                            folder_with_filtered_images
-                            )
-        print(f"Copied file {img_name} files {i}")
-        signs_csv_img_list.remove(img_name)
-print("COPYING SUCCESFULL !!")
+    for img_name in files_lable_list:
+        j += 1
+        print(f"Checked: {j} files")
+        
+        img_name = img_name[:-5]
+        img_json_name = img_name + ".json"
+        img_jpg_name = img_name + ".jpg"  
+        if img_name in signs_csv_img_list:
+            i += 1
+            shutil.copy(
+                    os.path.join(
+                                path_to_images, img_jpg_name), 
+                                folder_with_filtered_images
+                                )
+            shutil.copy(
+                    os.path.join(
+                                path_to_labels, img_json_name), 
+                                folder_with_filtered_images
+                                )
+            print(f"Copied file {img_name} files {i}")
+            signs_csv_img_list.remove(img_name)
+    print(f"Copying of {label_of_sign} SUCCESFULL!!")
--- find_signs_JSON.py
+++ find_signs_JSON.py
@@ -2,9 +2,18 @@ import json
 import os
 import csv
 
-# label_of_sign = "information--pedestrians-crossing--g1" #znak przejścia dla pieszych
-label_of_sign = "regulatory--stop--g1" #znak stop
-# label_of_sign = "regulatory--stop--g10" #znak stop2
+# label_of_sign = "information--pedestrians-crossing--g1" # znak przejścia dla pieszych
+# label_of_sign = "regulatory--stop--g1" # znak stop
+# label_of_sign = "regulatory--stop--g10" # znak stop2
+# label_of_sign = "regulatory--maximum-speed-limit-50--g1"  #znak ograniczenia predkosco do 50km/h
+# label_of_sign = "warning--pedestrians-crossing--g5" # znak przejscia dla pieszych ostrzeg. 
+# label_of_sign = "warning--railroad-crossing--g3" # przejazd kolejowy
+# label_of_sign = "warning--railroad-crossing-with-barriers--g1" # przejazd kolejowy z barierami 
+
+
+
+labels_list = [label_of_sign]
+
 
 print("1 - Find one choosen label\n")
 print("3 - Get label list \n")
@@ -22,33 +31,33 @@ respond = input("Chose one: ")
 
 while respond != "0":
     if respond == "1":
-        with open(f'output/imagelist_label_{label_of_sign}.csv', mode="w", newline="") as csv_file:
-            i = 1
-            new_row = csv.writer(
-                csv_file, 
-                delimiter=',',
-                quotechar='"',
-                quoting=csv.QUOTE_MINIMAL
-                )
-            for file_ in files_list:
-                i += 1
-                json_file = os.path.join(path_to_annotations, file_)
-                print(f"{i} / {len(files_list)}")
-                with open(json_file, "r") as f:
-                    steam = json.load(f)
-                    objects = steam['objects']
-                    for properties in objects:
-                        label = properties["label"]
-                        if label == label_of_sign and file_ not in images:
-                            file_ = file_[:-5]
-                                # file_ = file_ + ".jpg"
-                            images.append(file_)
-                            new_row.writerow([file_])
-                            print("FILE", file_)
-                            break 
-        respond = "0"
-        print("Number of images with choosen sign: ", len(images))
-        print(f"CSV file with image name has been save as 'imagelist_{label_of_sign}.csv'")
+        for label_of_sign in labels_list:
+            with open(f'output/imagelist_label_{label_of_sign}.csv', mode="w", newline="") as csv_file:
+                i = 1
+                new_row = csv.writer(
+                    csv_file, 
+                    delimiter=',',
+                    quotechar='"',
+                    quoting=csv.QUOTE_MINIMAL
+                    )
+                for file_ in files_list:
+                    i += 1
+                    json_file = os.path.join(path_to_annotations, file_)
+                    print(f"{i}/{len(files_list)} [{len(images)} found]")
+                    with open(json_file, "r") as f:
+                        steam = json.load(f)
+                        objects = steam['objects']
+                        for properties in objects:
+                            label = properties["label"]
+                            if label == label_of_sign and file_ not in images:
+                                file_ = file_[:-5]
+                                images.append(file_)
+                                new_row.writerow([file_])
+                                print("FILE", file_)
+                                break 
+            respond = "0"
+            print("Number of images with choosen sign: ", len(images))
+            print(f"CSV file with image name has been save as 'imagelist_{label_of_sign}.csv'")
     elif respond == "3":
         with open('output/label_list.csv', mode="w", newline="") as csv_file:
             i = 1
